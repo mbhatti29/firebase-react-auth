252c7cc93d9f8d96439ede1318c03a76f992d09a
Mudassar Bhatti <mbhatti000@citymail.cuny.edu>
2019-07-12 16:43:28 -0400

    secured apikey

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Firebase = () => {
   return (
     <div>
-
+    
     </div>
   );
 }
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SignIn = () => {
   return (
     <div>
-
+      <h1>Please Sign In</h1>
     </div>
   );
 }
